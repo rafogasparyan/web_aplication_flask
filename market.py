9a45cb1a00f2e909383f1b441990225869e23188
rafgasparyan <rgasparyan@griddynamics.com>
2024-11-07 16:38:06 +0400

    added styling and templates

--- market.py
+++ market.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "Hello world!  "
+@app.route("/home")
+def home_page():
+    return render_template("home.html")
+
