b770063847acb104937ea8ecbc27bbd1fb2c0b0e
rafgasparyan <rgasparyan@griddynamics.com>
2024-11-09 16:59:55 +0400

    Huge commit (Advanced validation, model relationship, Flask Forms, User Authentication(partially) are added)

--- market/__init__.py
+++ market/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.app_context().push()
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///market.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = '6d9a7a812296d7d87ca519e2'
 db = SQLAlchemy(app)
-
+bcrypt = Bcrypt(app)
 from market import routes
