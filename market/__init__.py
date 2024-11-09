1be342141470293705630dda6eb4b4ad107e7c99
rafgasparyan <rgasparyan@griddynamics.com>
2024-11-09 18:21:17 +0400

    User authentication part2 completed

--- market/__init__.py
+++ market/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.app_context().push()
@@ -8,4 +9,5 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = '6d9a7a812296d7d87ca519e2'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 from market import routes
