7e91d6c87b62c1fb824fbc03992f3c3fc93dd659
rafgasparyan <rgasparyan@griddynamics.com>
2024-11-09 21:44:22 +0400

    Purchase Item completed

--- market/models.py
+++ market/models.py
@@ -33,6 +33,9 @@ class User(db.Model, UserMixin):
     def check_password_correction(self, attempted_password):
         return bcrypt.check_password_hash(self.password_hash, attempted_password)
 
+    def can_purchase(self, item_obj):
+        return self.budget >= item_obj.price
+
 
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
@@ -44,6 +47,11 @@ class Item(db.Model):
 
     def __repr__(self):
         return f"Item {self.name}"
+
+    def buy(self, user):
+        self.owner = user.id
+        user.budget -= self.price
+        db.session.commit()
 
 
 # u1 = User(username="jsc", password_hash="pass", email="abc@mail.com")
